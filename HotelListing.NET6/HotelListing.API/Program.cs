1db21eed5e3495277d1df00e8778dbd703379d16
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-07-23 15:29:01 -0300

    Configure Serilog And Seq

--- HotelListing.NET6/HotelListing.API/Program.cs
+++ HotelListing.NET6/HotelListing.API/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +9,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(option =>
-{
-    option.AddPolicy("AllowAll", b => b.AllowAnyHeader()
-                                       .AllowAnyOrigin()
-                                       .AllowAnyMethod());
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyHeader()
+            .AllowAnyOrigin()
+            .AllowAnyMethod());
+});
+
+//builder.Host.UseSerilog((ctx, services, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+builder.Host.UseSerilog((hostContext, configuration) => {
+    configuration.WriteTo.Console().ReadFrom.Configuration(hostContext.Configuration);
 });
 
 var app = builder.Build();
